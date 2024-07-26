428f6ce4f05f54d1fc068e0a950fab7c876b6934
Shaum <105980825+Astroneural@users.noreply.github.com>
2024-07-25 22:19:55 -0500

    Update capture_video.py

--- capture_video.py
+++ capture_video.py
@@ -1,5 +1,5 @@
 import cv2
-video_capture = cv2.VideoCapture('carina.mp4')
+video_capture = cv2.VideoCapture('/Path/To/file.mp4')
 frame_number = 0
 
 while True:
@@ -8,7 +8,7 @@ while True:
         break
 
     # Save the frame as an image
-    frame_filename = f'/Users/shaum/space-sonification/carina_frames/frame_{frame_number:04d}.jpg'  # Adjust the filename format
+    frame_filename = f'/Path/To/file_frames/frame_{frame_number:04d}.jpg' # create the folder file_frames (e.g. orion_nebula_frames/)
     cv2.imwrite(frame_filename, frame)
     frame_number += 1
 video_capture.release()
