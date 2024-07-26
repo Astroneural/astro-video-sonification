87e4424dd806b1e70166ac066a4a74701d20c0e6
Shaum <105980825+Astroneural@users.noreply.github.com>
2024-07-25 22:13:08 -0500

    Update sonify copy 3.py

--- sonify copy 3.py
+++ sonify copy 3.py
@@ -6,63 +6,53 @@ import numpy as np
 from midi2audio import FluidSynth
 from moviepy.editor import VideoFileClip, AudioFileClip
 
-folder_path = "/Users/shaum/space-sonification/carina_frames"
+folder_path = "/Path/To/Frames" # outputted by capture_video.py
 file_list = os.listdir(folder_path)
 frame_number = len(file_list)
 
 # Variables to tinker: min_object_sizes, brightness_threshold, triggered_box_size, cooldown_period, circle_radius
 
-fps = 25
+fps = 25 # as of this version, must be manually checked
 
-# Define the radius of the circle
 circle_radius = 350
 
-# Define the minimum size of the detected objects
-min_object_size = 5  # 10
+min_object_size = 5
 
-brightness_threshold = 75  # 150
+brightness_threshold = 75
 
-# Initialize a list to store the frames with circles drawn
 frames_with_circles = []
 
-# Initialize a list to store the frames with detected objects boxed
 frames_with_boxes = []
 
-# Generate MIDI notes for detected objects
 twinkle_notes = [60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94]  # C wholetone
 
 midi = pretty_midi.PrettyMIDI()
 
-# Create instrument objects
-instrument_harp = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Ocarina'))  # Pizzicato Strings
+instrument_harp = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Ocarina'))  # list available at https://github.com/craffel/pretty-midi/blob/main/pretty_midi/constants.py
 instrument_strings = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Pad 4 (choir)'))
 
 triggered_box_size = 5
 cooldown_period = 0.3
 triggered_regions = []
 
-# Loop through each frame
-for i in range(frame_number):
+for i in range(frame_number): # loop through each frame
     filename = os.path.join(folder_path, f"frame_{i:04d}.jpg")
     image = cv2.imread(filename)
     
     if image is None:
         continue
     
-    # Draw a circle on the frame
-    circle_center = (image.shape[1] // 2, image.shape[0] // 2)
+    circle_center = (image.shape[1] // 2, image.shape[0] // 2) # draw a circle
     cv2.circle(image, circle_center, circle_radius, (0, 50, 0), 2)
     
-    # Find bright objects on the circumference of the circle
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, threshold_image = cv2.threshold(gray_image, brightness_threshold, 255, cv2.THRESH_BINARY)
+    _, threshold_image = cv2.threshold(gray_image, brightness_threshold, 255, cv2.THRESH_BINARY) # find bright objects
     contours, _ = cv2.findContours(threshold_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    # Filter contours that are on the circumference of the circle
     for contour in contours:
         for point in contour:
             dist_to_center = np.linalg.norm(np.array(point[0]) - np.array(circle_center))
-            if abs(dist_to_center - circle_radius) < 10:  # Adjust the threshold as needed
+            if abs(dist_to_center - circle_radius) < 10: # find objects on/near the circle's circumference
                 x, y, w, h = cv2.boundingRect(contour)
                 object_location = (x + w // 2, y + h // 2)
                 object_size = max(w, h)
@@ -85,8 +75,8 @@ for i in range(frame_number):
                                 is_duplicate = True
                                 break
 
-                    if not is_duplicate:
-                        if object_size < 50:
+                    if not is_duplicate: # counter annoying pulsing noise when large objects remain on the circumference
+                        if object_size < 50: # for stars
                             instrument = instrument_harp
                             brightness_normalized = 1 - (object_brightness / 255.0)
                             note_index = int(brightness_normalized * (len(twinkle_notes) - 1))
@@ -100,12 +90,9 @@ for i in range(frame_number):
                             volume = 20 * np.log10(object_size / image.shape[0]) + 100
 
                         if instrument == instrument_harp:
-                            # Check if the previous note is the same as the current note
                             if len(instrument.notes) > 0 and instrument.notes[-1].pitch == pitch:
-                                # Extend the duration of the previous note
-                                instrument.notes[-1].end = time_stamp + 0.2
+                                instrument.notes[-1].end = time_stamp + 0.2 # allow for elongation
                             else:
-                                # Create a new note
                                 note = pretty_midi.Note(
                                     velocity=int(volume),
                                     pitch=int(pitch),
@@ -137,7 +124,6 @@ for i in range(frame_number):
     frames_with_circles.append(image.copy())
     frames_with_boxes.append(image.copy())
 
-# Save the frames with circles drawn as a video
 timestr = time.strftime("%H-%M-%S")
 video_path = 'raw_videos/frames_with_boxes_' + timestr + '.mp4'
 box_video = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (image.shape[1], image.shape[0]))
@@ -145,7 +131,6 @@ for frame in frames_with_boxes:
     box_video.write(frame)
 box_video.release()
 
-# Add the instruments to the MIDI object
 midi.instruments.append(instrument_harp)
 midi.instruments.append(instrument_strings)
 
@@ -158,8 +143,7 @@ def convert_midi_to_wav(midi_file, output_file, soundfont_path):
 
 midi_file = midi_path
 output_file = midi_path + '.wav'
-# soundfont_path = '/Users/shaum/Downloads/GeneralUser GS 1.471/GeneralUser GS v1.471.sf2'
-soundfont_path = '/Users/shaum/Downloads/TimGM6mb.sf2'  # Update this to the path where you saved the SoundFont
+soundfont_path = 'Path/to/TimGM6mb.sf2'  # download at https://github.com/craffel/pretty-midi/blob/main/pretty_midi/TimGM6mb.sf2
 convert_midi_to_wav(midi_file, output_file, soundfont_path)
 
 video = VideoFileClip(video_path)
